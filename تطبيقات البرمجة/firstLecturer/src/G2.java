e1d2238a9c2381beedb29b49ae2bd7a74f41be1a
khawlaalbahry <97443140+Khawlaalbahry@users.noreply.github.com>
2022-01-29 22:27:55 +0300

    Delete G2.java

--- تطبيقات البرمجة/firstLecturer/src/G2.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by lenovo on 09/01/2022.
- */
-public class G2 {
-    public static void main(String[] args) {
-        for (int i=1; i<=10 ; i++)
-            System.out.println(i);
-    }
-}
